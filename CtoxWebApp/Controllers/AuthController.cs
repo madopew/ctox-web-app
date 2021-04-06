7500e0d17cf0bd2e40f6163ff0fb2ba9f11bcef7
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-04-06 15:14:27 +0300

    Add login page

--- CtoxWebApp/Controllers/AuthController.cs
+++ CtoxWebApp/Controllers/AuthController.cs
@@ -1,22 +1,47 @@
-﻿using System.Linq;
-using CtoxWebApp.DAL;
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using CtoxWebApp.Models;
+using CtoxWebApp.Services;
 using Microsoft.AspNetCore.Mvc;
+using AppContext = CtoxWebApp.DAL.AppContext;
 
 namespace CtoxWebApp.Controllers
 {
     public class AuthController : Controller
     {
-        private AppContext context;
+        private readonly AppContext context;
+        private readonly HashService hashService;
         
-        public AuthController(AppContext context)
+        public AuthController(AppContext context, HashService hashService)
         {
             this.context = context;
+            this.hashService = hashService;
         }
 
-        // GET
-        public IActionResult Index()
+        public IActionResult Login()
         {
-            return View(context.Users.First());
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserLogin user)
+        {
+            var hash = hashService.GetHash(string.Concat(user.Username, hashService.GetHash(user.Password)));
+            
+            var result = context.Users
+                .FirstOrDefault(u => u.Username.Equals(user.Username, StringComparison.Ordinal) &&
+                                     u.PasswordHash.Equals(hash, StringComparison.Ordinal));
+
+            if (result is null)
+            {
+                ViewData["error-message"] =
+                    "We couldn't find an account matching the username and password you entered. Please check your username and password and try again.";
+                return View();
+            }
+
+            return null;
         }
     }
 }
