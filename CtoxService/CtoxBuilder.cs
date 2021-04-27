b7dc5d2c64475616a47e9564e5fd060058a88315
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-04-27 20:27:49 +0300

    Rename ctox project

--- CtoxService/CtoxBuilder.cs
+++ CtoxService/CtoxBuilder.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
-namespace Ctox
+namespace CtoxService
 {
     public class CtoxBuilder
     {
--- CtoxWebApp/Services/Interfaces/IParseService.cs
+++
@@ -1,7 +0,0 @@
-namespace CtoxWebApp.Services.Interfaces
-{
-    public interface IParseService
-    {
-        string Parse(string content);
-    }
-}
