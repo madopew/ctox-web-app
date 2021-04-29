fe89f16b654b3489a3ac020629b0d611e1884157
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-04-29 20:07:21 +0300

    Add roles to api key filter

--- CtoxWebApp/Attributes/Filters/ApiKey.cs
+++ CtoxWebApp/Attributes/Filters/ApiKey.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data.Entity;
 using System.Linq;
 using CtoxWebApp.DAL;
 using Microsoft.AspNetCore.Mvc;
@@ -9,13 +10,12 @@ namespace CtoxWebApp.Attributes.Filters
 {
     public class ApiKey : Attribute, IActionFilter
     {
-        private AppDbContext dbContext;
-
-        public ApiKey(AppDbContext dbContext)
+        public ApiKey()
         {
-            this.dbContext = dbContext;
         }
-        
+
+        public string Roles { get; set; }
+
         public void OnActionExecuting(ActionExecutingContext context)
         {
             // ReSharper disable once PossibleNullReferenceException
@@ -33,11 +33,32 @@ namespace CtoxWebApp.Attributes.Filters
                 return;
             }
 
-            var exists = dbContext.Apis.Any(a => a.Key.Equals(context.HttpContext.Request.Headers["x-api-key"]));
-            if (!exists)
+            var dbContext = (AppDbContext) context.HttpContext.RequestServices.GetService(typeof(AppDbContext));
+
+            var api = dbContext.Apis
+                .Include(a => a.User)
+                .FirstOrDefault(a => a.Key.Equals(context.HttpContext.Request.Headers["x-api-key"]));
+
+            if (api is null)
             {
                 context.Result = new UnauthorizedResult();
+                return;
             }
+
+            if (Roles is null || Roles.Length == 0)
+            {
+                return;
+            }
+
+            foreach (var role in Roles.Split(','))
+            {
+                if (api.User.Role.ToString().Equals(role))
+                {
+                    return;
+                }
+            }
+
+            context.Result = new UnauthorizedResult();
         }
 
         public void OnActionExecuted(ActionExecutedContext context)
