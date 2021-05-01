e2cac0b92e2287c57075c51aa2306434efaf5b82
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-05-01 13:14:15 +0300

    Remove migrations from gitignore

---
+++ CtoxWebApp/Migrations/20210410182858_AddApi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CtoxWebApp.Migrations
+{
+    public partial class AddApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CtoxWebApp/Migrations/20210411193719_AddApiLastUsed.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CtoxWebApp.Migrations
+{
+    public partial class AddApiLastUsed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Left",
+                table: "Apis");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUsed",
+                table: "Apis",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUsed",
+                table: "Apis");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Left",
+                table: "Apis",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
